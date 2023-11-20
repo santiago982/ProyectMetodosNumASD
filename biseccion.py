7384f344011d362a44788b2f96c03d1c8fd9c1b5
sergimesa <149121899+sergimesa@users.noreply.github.com>
2023-11-19 21:53:06 -0500

    Update biseccion.py

--- biseccion.py
+++ biseccion.py
@@ -1,60 +1,62 @@
-def bisection_method(func, a, b, tolerance=1e-6, max_iterations=100):
-    """
-    Método de bisección para encontrar la raíz de una función.
+# -*- coding: utf-8 -*-
 
-    Parameters:
-    - func: La función para la cual se busca la raíz.
-    - a, b: Extremos del intervalo [a, b] donde se realizará la búsqueda.
-    - tolerance: Tolerancia para la convergencia (valor predeterminado: 1e-6).
-    - max_iterations: Número máximo de iteraciones (valor predeterminado: 100).
+import numpy as np
+import matplotlib.pyplot as plt
 
-    Returns:
-    - root: Aproximación de la raíz.
-    - iterations: Número de iteraciones realizadas.
-    """
+def get_user_input():
+    # Solicitar la función al usuario
+    expression = input("Ingrese la función (utilice 'x' como variable): ")
+    # Crear la función a partir de la expresión proporcionada
+    func = lambda x: eval(expression)
+    
+    # Solicitar los extremos del intervalo al usuario
+    a = float(input("Ingrese el extremo izquierdo del intervalo: "))
+    b = float(input("Ingrese el extremo derecho del intervalo: "))
+    
+    return func, a, b
 
-    # Verificar si los extremos del intervalo tienen signos opuestos
-    if func(a) * func(b) > 0:
-        raise ValueError("Los extremos del intervalo deben tener signos opuestos.")
+def bisection_method(func, a, b, tol=1e-6, max_iter=100):
+    if func(a) * func(b) >= 0:
+        raise ValueError("La función debe tener signos opuestos en los extremos del intervalo.")
 
-    # Inicializar variables
-    iteration = 0
-    root = None
-
-    # Bucle de iteraciones
-    while iteration < max_iterations:
-        # Calcular el punto medio del intervalo
+    num_iterations = 0
+    while (b - a) / 2 > tol and num_iterations < max_iter:
         c = (a + b) / 2
-
-        # Verificar si c es la raíz o si la tolerancia se ha alcanzado
-        if abs(func(c)) < tolerance:
-            root = c
+        if func(c) == 0:
             break
-
-        # Actualizar el intervalo [a, b]
         if func(c) * func(a) < 0:
             b = c
         else:
             a = c
+        num_iterations += 1
 
-        # Incrementar el contador de iteraciones
-        iteration += 1
+    root = (a + b) / 2
+    return root, num_iterations
 
-    return root, iteration
+def plot_function_and_root(func, a, b, root):
+    x_vals = np.linspace(a, b, 100)
+    y_vals = func(x_vals)
 
-# Ejemplo de uso
-def example_function(x):
-    return x**2 - 4
+    plt.plot(x_vals, y_vals, label='f(x)')
+    plt.axhline(0, color="red")
+    plt.axvline(0, color="green")
+    plt.scatter(root, func(root), color='blue', label='Root')
+    plt.xlabel('eje x')
+    plt.ylabel('eje y')
+    plt.legend()
+    plt.grid(True)
+    plt.title('Bisección')
+    plt.show()
 
-# Intervalo inicial [a, b]
-a, b = 0, 3
+def main():
+    func, a, b = get_user_input()
+    root, num_iterations = bisection_method(func, a, b)
 
-# Llamada al método de bisección
-root, iterations = bisection_method(example_function, a, b)
-
-# Mostrar resultados
-if root is not None:
     print(f"Aproximación de la raíz: {root}")
-    print(f"Número de iteraciones: {iterations}")
-else:
-    print("El método de bisección no convergió.")
+    print(f"Número de iteraciones: {num_iterations}")
+
+    plot_function_and_root(func, a, b, root)
+
+if __name__ == "__main__":
+    main()
+
