450dc85c0ebfcdda3c1cb66fca0ce2f43cb82d57
santiago982 <63021557+santiago982@users.noreply.github.com>
2023-12-13 20:59:32 -0500

    Agregue al metodo punto fijo la documentacion y recuperacion borrado accidental

--- Punto Fijo.py
+++ Punto Fijo.py
@@ -1,111 +1,98 @@
-import tkinter as tk
-from tkinter import ttk
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
-import numpy as np
+import matplotlib.pyplot as plt #libreria de graficar
+import numpy as np 
+#libreria de uso y aplicacion de funciones y operaciones matematicas
 
-xi = []
-fx_input = ""
-gx_input = ""
+ 
+
+
+xi = []   # Lista para almacenar las aproximaciones sucesivas
+
+# gx: La función de punto fijo.
+# x0: El valor inicial.
+# t: La tolerancia para la aproximación de la raíz,
+# (valor por defecto: 0.0001).
+#  itMax: Número máximo de iteraciones (valor por defecto: 100).
 
 
 def puntofijo(gx, x0, t=0.0001, itMax=100):
-    global xi
-    xi = []
-    it = 0
-    x1 = gx(x0)
-    intervalo = abs(x1 - x0)
-    while intervalo >= t and it <= itMax:
-        x0 = x1
-        x1 = gx(x0)
-        it += 1
-        intervalo = abs(x1 - x0)
-        xi.append(x1)
-    raiz = x1
-    if it >= itMax:
-        raiz = np.nan
-    return raiz
+    it = 0       # iteraciones contador
+    x1 = gx(x0)  # Calcular la primera aproximación
+    intervalo = abs(x1-x0)   # Calcular el intervalo entre aproximaciones
+    while (intervalo >= t and it <= itMax): # Iterar mientras se cumplan las condiciones de convergencia y el número máximo de iteraciones no se haya alcanzado
+        x0 = x1              # Iterar mientras se cumplan las condiciones de convergencia y el número máximo de iteraciones no se haya alcanzado
+        x1 = gx(x0)          # Calcular la siguiente aproximación
+        it = it + 1          # Incrementar el contador de iteraciones
+        intervalo = abs(x1-x0)   # Calcular el nuevo intervalo
+        
+        
+        print("Iteracion: ", it, " Raiz: ", x1, " Intervalo: ", intervalo)
+        xi.append(x1)    # Agregar la nueva aproximación a la lista
+
+    raiz = x1              # Establecer el resultado como la última aproximación
+    if (it >= itMax):      # Verificar si se alcanzó el número máximo de iteraciones
+        raiz = np.nan      # Si sí, establecer el resultado como NaN (no es un número)
+    return raiz            #devuelve resultado
 
 
-def fx(x):
-    return eval(fx_input)
+# PROGRAMA -----------------
+# INGRESO---------Datos
+fx_input = input("Ingrese la función f(x): ") #funcion inicial 
+gx_input = input("Ingrese la función g(x): ") #su despeje
+x0 = float(input("Ingrese el valor inicial x0: ")) #Punto Inicial
+tolera = float(
+    input("Ingrese la tolerancia para la aproximación de la raíz: "))
+
+# Pruebas
+# 2*x**2 - x - 5
+# (x + 5) / 2
+
+# -x**2+1/3-1/9*x
+#  1/3-2/9*x 
 
 
-def gx(x):
-    return np.sqrt(eval(gx_input))
+# Definir las funciones f(x) y g(x) a partir de las entradas del usuario
 
 
-def calcular_raiz():
-    global xi, fx_input, gx_input
-    xi = []
-    fx_input = fx_entry.get()
-    gx_input = gx_entry.get()
-    x0 = float(x0_entry.get())
-    tolera = float(tolera_entry.get())
-
-    respuesta = puntofijo(gx, x0, tolera)
-
-    # Limpiar el gráfico anterior
-    for widget in frame_grafico.winfo_children():
-        widget.destroy()
-
-    # Crear un rango de valores x desde -3 hasta 3
-    x = np.linspace(-3, 3, 100)
-    y = fx(x)
-    raiz = respuesta
-    y_punto = fx(raiz)
-
-    # Crear el gráfico
-    fig = Figure(figsize=(5, 4), dpi=100)
-    plot = fig.add_subplot(1, 1, 1)
-    plot.plot(x, y, label='f(x) = ' + fx_input)
-    plot.axhline(y=0, color='black', linestyle='--')
-    plot.axvline(x=0, color='black', linestyle='--')
-    for i in range(len(xi)):
-        plot.scatter(xi[i], 0, color='blue', marker='.', label='Iteracion ' + str(i + 1))
-    plot.scatter(raiz, y_punto, marker='*', color='red', label='Raíz')
-    plot.set_xlabel('x')
-    plot.set_ylabel('f(x)')
-    plot.legend()
-    plot.grid(True)
-
-    # Agregar el gráfico a la interfaz
-    canvas = FigureCanvasTkAgg(fig, master=frame_grafico)
-    canvas_widget = canvas.get_tk_widget()
-    canvas_widget.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+def fx(x): return eval(fx_input)
+def gx(x): return np.sqrt(eval(gx_input))
 
 
-# Crear la interfaz gráfica
-root = tk.Tk()
-root.title("Método del Punto Fijo")
+b = 0      # intervalo
+# tolera = 0.001
+iteramax = 100  # iteraciones máximas
 
-# Crear y configurar el frame principal
-frame_principal = ttk.Frame(root, padding="10")
-frame_principal.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+# PROCEDIMIENTO para Aplicar el método del punto fijo
+respuesta = puntofijo(gx, x0, tolera)
+print(xi)     # Imprimir la lista de aproximaciones
 
-# Etiquetas y campos de entrada
-ttk.Label(frame_principal, text="Función f(x):").grid(row=0, column=0, sticky=tk.W)
-fx_entry = ttk.Entry(frame_principal)
-fx_entry.grid(row=0, column=1, sticky=tk.W)
 
-ttk.Label(frame_principal, text="Función g(x):").grid(row=1, column=0, sticky=tk.W)
-gx_entry = ttk.Entry(frame_principal)
-gx_entry.grid(row=1, column=1, sticky=tk.W)
+# SALIDA
+print(respuesta)
 
-ttk.Label(frame_principal, text="Valor inicial x0:").grid(row=2, column=0, sticky=tk.W)
-x0_entry = ttk.Entry(frame_principal)
-x0_entry.grid(row=2, column=1, sticky=tk.W)
+# Gráfico
+# Crear un rango de valores x desde -10 hasta 10
+x = np.linspace(-3, 3, 100)
 
-ttk.Label(frame_principal, text="Tolerancia:").grid(row=3, column=0, sticky=tk.W)
-tolera_entry = ttk.Entry(frame_principal)
-tolera_entry.grid(row=3, column=1, sticky=tk.W)
+# Evaluar la función para cada valor de x
+y = fx(x)
+raiz = respuesta
+y_punto = fx(raiz)
 
-# Botón de calcular
-calcular_button = ttk.Button(frame_principal, text="Calcular Raíz", command=calcular_raiz)
-calcular_button.grid(row=4, column=0, columnspan=2, pady=10)
 
-# Frame para el gráfico
-frame_grafico = ttk.Frame(root)
-frame_grafico.grid(row=1, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+# Crear el gráfico
+plt.title('Gráfico de la función f(x)')
+plt.plot(x, y, label='f(x) = '+fx_input)
+plt.axhline(y=0, color='black', linestyle='--')
+plt.axvline(x=0, color='black', linestyle='--')
+for i in range(len(xi)):
+    plt.scatter(xi[i], 0, color='blue', marker='.',
+                label='Iteracion '+str(i+1))
+plt.scatter(raiz, y_punto, marker='*', color='red', label='Raíz')
 
-root.mainloop()
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.legend()
+plt.grid(True)
+
+# Mostrar el gráfico
+plt.show()
