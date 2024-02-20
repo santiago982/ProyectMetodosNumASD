55ec3f7f58dcd6a25de50becc8b4fa5cab331338
santiago982 <63021557+santiago982@users.noreply.github.com>
2024-02-19 22:09:19 -0500

    pequeña documentacion seccion metodo

--- MetodoSymson2.py
+++ MetodoSymson2.py
@@ -1,13 +1,13 @@
-import sympy as sp
-import matplotlib.pyplot as plt
-import numpy as np
+import sympy as sp                 #para convertir una expresión simbólica en una función numérica
+import matplotlib.pyplot as plt  #libreria para Graficar
+import numpy as np               # para operaciones numericas y manipulacion de arreglos
 
 def convertir_a_funcion(expr):
     x = sp.Symbol('x')
     return sp.lambdify(x, sp.sympify(expr), "numpy")
 
 def calcular_error_simpson(funcion, a, b, n):
-    x = sp.Symbol('x')
+    x = sp.Symbol('x')   # Se define x 
     cuarta_derivada = sp.diff(funcion, x, 4)
     K = max([cuarta_derivada.subs(x, val) for val in np.linspace(a, b, 1000)])
     error = (K * (b - a)**5) / (180 * n**4)
@@ -16,22 +16,22 @@ def calcular_error_simpson(funcion, a, b, n):
 
 def simpson(funcion, a, b, n):
     if n == 2:
-        h = (b - a) / 6
-        valores = np.linspace(a, b, n+1)
+        h = (b - a) / 6  #Calcula el ancho de cada subintervalo.
+        valores = np.linspace(a, b, n+1)  # espaciado en puentos intermedios
         resultado = h * \
-            (funcion(valores[0]) + 4*funcion(valores[1]) + funcion(valores[2]))
+            (funcion(valores[0]) + 4*funcion(valores[1]) + funcion(valores[2]))# Calcula la aproximación de la integral utilizando la fórmula de Simpson simple. Multiplica el ancho del subintervalo por la suma ponderada de los  valores de la función en los extremos y el punto medio del intervalo.
         print(f"El resultado del método es = {round(resultado, 5)}")
         return resultado
-
+# si es mas de 2 usa la formula 2 o caso2
     elif n > 2 and n % 2 == 0:
-        h = (b - a) / (3 * n)
-        valores = np.linspace(a, b, n+1)
-        sumatoria_xi = funcion(valores[0]) + funcion(valores[-1])
-        sumatoria_mi = sum([2 * funcion(valores[i]) for i in range(1, n, 2)])
-        sumatoria_mi += sum([4 * funcion(valores[i]) for i in range(2, n-1, 2)])
-        resultado = h * (sumatoria_xi + sumatoria_mi)
+        h = (b - a) / (3 * n) # Calcula el ancho de cada subintervalo para el método compuesto.
+        valores = np.linspace(a, b, n+1) # Calcula el ancho de cada subintervalo para el método compuesto.
+        sumatoria_xi = funcion(valores[0]) + funcion(valores[-1])  # Calcula la suma de los valores de la función en los extremos del intervalo.
+        sumatoria_mi = sum([2 * funcion(valores[i]) for i in range(1, n, 2)]) #  Calcula la suma de los valores de la función multiplicados por 2 en los puntos intermedios impares.
+        sumatoria_mi += sum([4 * funcion(valores[i]) for i in range(2, n-1, 2)])  # Suma los valores de la función multiplicados por 4 en los puntos intermedios pares.
+        resultado = h * (sumatoria_xi + sumatoria_mi) #  Calcula la aproximación de la integral utilizando la fórmula compuesta del método de Simpson.
         print(f"El resultado del método es = {round(resultado, 5)}")
-        return resultado
+        return resultado # regresa resultado
 
     else:
         print("Para el caso n > 2, n debe ser un número par.")
@@ -86,4 +86,4 @@ if resultado is not None:
 
     ax.legend()
 
-    plt.show()
+    plt.show()# pintar grafico area bajo la curva
