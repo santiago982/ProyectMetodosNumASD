08b6704a0beba8dd03bee0cdbc270bc7f3d1ac00
srr0bot <danielscmo07@gmail.com>
2024-02-20 15:37:35 -0500

    documentación

--- jacobi.py
+++ jacobi.py
@@ -31,18 +31,19 @@ def metodo_jacobi(matriz_ecuaciones, val_indep_ecuaciones, x_inicial, tolerancia
 
     """
 
-    D = np.diag(np.diag(matriz_ecuaciones))
-    LU = matriz_ecuaciones - D
+    D = np.diag(np.diag(matriz_ecuaciones)) # Extrae la diagonal de la matriz de coeficientes matriz_ecuaciones y crea una matriz diagonal D con esos elementos.
+    LU = matriz_ecuaciones - D # Calcula la matriz LU restando la matriz diagonal D de la matriz de coeficientes matriz_ecuaciones.
     x = x_inicial
     for i in range(N):
-        D_inv = np.diag(1 / np.diag(D))
-        x_nuevo = np.dot(D_inv, val_indep_ecuaciones - np.dot(LU, x))
-        diff_norm = np.linalg.norm(x_nuevo - x)
+        D_inv = np.diag(1 / np.diag(D)) # Calcula la inversa de la matriz diagonal D y crea una matriz diagonal D_inv con esos elementos.
+        x_nuevo = np.dot(D_inv, val_indep_ecuaciones - np.dot(LU, x)) # Calcula el nuevo vector x_nuevo utilizando el método de Jacobi.
+        diff_norm = np.linalg.norm(x_nuevo - x) # Calcula la norma de la diferencia entre el vector x_nuevo y el vector x de la iteración anterior.
         if diff_norm < tolerancia:
             return x_nuevo
+        # Comprueba si la norma de la diferencia es menor que la tolerancia. Si es así, devuelve el vector x_nuevo como solución.
         x = x_nuevo
         print(
-            f"Iteración {i+1}: x = {np.round(x, 5)}")
+            f"Iteración {i+1}: x = {np.round(x, 5)}") # Imprime información sobre la iteración actual incluyendo el número de iteración y el vector resultante x.
     return x
 
 # Solicitar la entrada del usuario
@@ -50,6 +51,7 @@ n = int(input("Introduce el número de ecuaciones: "))
 matriz_ecuaciones = np.zeros((n, n))
 val_indep_ecuaciones = np.zeros(n)
 x_inicial = np.zeros(n)
+# Inicializa matrices y vectores de ceros para almacenar la matriz de coeficientes, el vector de términos independientes y el vector de valores iniciales.
 
 # Obtener la matriz de coeficientes, términos independientes y valores iniciales
 for i in range(n):
