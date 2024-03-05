e14da5a119d244fce54b17856e32a89bc7f2bed8
santiago982 <63021557+santiago982@users.noreply.github.com>
2024-03-05 15:08:19 -0500

    Documentacion y cambio en la formula de   h = (b - a) / (3 * n) usando mejor h = (b - a) / n y mas abajo para cumplir con la formula se usando  resultado = h / 3 * (sumatoria_xi + sumatoria_mi) Donde se corrige el error de mas de un trapecio
    modified:   MetodoSimpson2.py

--- MetodoSimpson2.py
+++ MetodoSimpson2.py
@@ -16,20 +16,22 @@ def calcular_error_simpson(funcion, a, b, n):
 
 def simpson(funcion, a, b, n):
     if n == 2:
-        h = (b - a) / 6
-        valores = np.linspace(a, b, n+1)
-        resultado = h * (funcion(valores[0]) + 4*funcion(valores[1]) + funcion(valores[-1]))
+        h = (b - a) / 6  #Calcula el ancho de cada subintervalo.
+        valores = np.linspace(a, b, n+1)  # espaciado en puentos intermedios
+        resultado = h * (funcion(valores[0]) + 4*funcion(valores[1]) + funcion(valores[-1]))# Calcula la aproximación de la integral utilizando la fórmula de Simpson simple. Multiplica el ancho del subintervalo por la suma ponderada de los  valores de la función en los extremos y el punto medio del intervalo.
         print(f"El resultado del método es = {round(resultado, 5)}")
         return resultado
+    
+     # si es mas de 2 usa la formula 2 o caso2
     elif n > 2 and n % 2 == 0:
-        h = (b - a) / n
-        valores = np.linspace(a, b, n+1)
-        sumatoria_xi = funcion(valores[0]) + funcion(valores[-1])
-        sumatoria_mi = sum([2 * funcion(valores[i]) for i in range(1, n, 2)])
-        sumatoria_mi += sum([4 * funcion(valores[i]) for i in range(2, n-1, 2)])
-        resultado = h / 3 * (sumatoria_xi + sumatoria_mi)
+        h = (b - a) / n  # Calcula el ancho de cada subintervalo para el método compuesto.
+        valores = np.linspace(a, b, n+1)  # Calcula el ancho de cada subintervalo para el método compuesto.
+        sumatoria_xi = funcion(valores[0]) + funcion(valores[-1]) # Calcula la suma de los valores de la función en los extremos del intervalo.
+        sumatoria_mi = sum([2 * funcion(valores[i]) for i in range(1, n, 2)]) #  Calcula la suma de los valores de la función multiplicados por 2 en los puntos intermedios impares.
+        sumatoria_mi += sum([4 * funcion(valores[i]) for i in range(2, n-1, 2)])  # Suma los valores de la función multiplicados por 4 en los puntos intermedios pares.
+        resultado = h / 3 * (sumatoria_xi + sumatoria_mi) #  Calcula la aproximación de la integral utilizando la fórmula compuesta del método de Simpson.
         print(f"El resultado del método es = {round(resultado, 5)}")
-        return resultado
+        return resultado # regresa resultado
     else:
         print("Para el caso n > 2, n debe ser un número par.")
         return None
